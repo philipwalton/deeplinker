d25a063025f33d31002523004589715a0d0fc22a
grano <andy.granowitz@gmail.com>
2015-02-24 15:06:38 -0800

    update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 chrome.tabs.getSelected(null, function(tab) {
   var deepLink = tab.url.replace(/\a[\d]+w[\d]+p[\d]+/, 'ACCOUNT_ID').substr(38)
   document.getElementById('content').innerHTML = 
-  'http://www.ga-dev-tools.com/r?ga=' + deepLink;
+  'localhost:8080/deeplinker/?ga=' + deepLink;
 });
