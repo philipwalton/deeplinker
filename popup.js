342928214ef3d4d3be9a16601eb54b449ecb962b
grano <andy.granowitz@gmail.com>
2015-03-18 15:52:59 -0700

    add title field

--- popup.js
+++ popup.js
@@ -1,5 +1,19 @@
 chrome.tabs.getSelected(null, function(tab) {
   var deepLink = tab.url.replace(/\a[\d]+w[\d]+p[\d]+/, 'ACCOUNT_ID').substr(38)
-  document.getElementById('content').innerHTML = 
+  var link = document.getElementById('link');
+  link.addEventListener('click', function() {
+    this.select();
+  })
+  link.innerHTML = 
   'localhost:8080/deeplinker/?ga=' + deepLink;
+  document.getElementById('title').value = 'hi';
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.greeting == "hello")
+        sendResponse({farewell: "goodbye"});
+    });
 });
+
