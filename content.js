43bf9d84a6b087166d70172c2c3b450bf71f16da
grano <andy.granowitz@gmail.com>
2015-03-19 15:51:37 -0700

    add title functionality

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
-console.log('im here');
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    /* If the received message has the expected format... */
+    if (msg.text && (msg.text == "report_back")) {
+        /* Call the specified callback, passing 
+           the web-pages DOM content as argument */
+    sendResponse(document.getElementsByClassName('_GAt8b')[0].innerHTML);
+    }
+});
--- popup.js
+++ popup.js
@@ -4,16 +4,44 @@ chrome.tabs.getSelected(null, function(tab) {
   link.addEventListener('click', function() {
     this.select();
   })
-  link.innerHTML = 
-  'localhost:8080/deeplinker/?ga=' + deepLink;
-  document.getElementById('title').value = 'hi';
-  chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting == "hello")
-        sendResponse({farewell: "goodbye"});
-    });
+  var title = document.getElementById('title');
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    title.value = tabs[0].title.substr(0, tabs[0].title.length - 19);
+    updateLink(link, deepLink, title.value);
+  });
+  title.addEventListener('input', function() {
+    updateLink(link, deepLink, title.value);
+  });
+  document.getElementById('shortlink').addEventListener('click', function() {
+    if(this.checked) {
+      updateLink(link, deepLink, title.value, true);
+    }
+    else {
+      updateLink(link, deepLink, title.value, false);
+    }
+
+  });
+  function updateLink(link, deepLink, title, shortlink) {
+    shortlink = shortlink || false;
+    var fullUrl = 'localhost:8080/deeplinker/?ga=' + deepLink +
+    '&title=' + title;
+    if(shortlink) {
+      link.innerHTML = shortenLink(fullUrl);
+    }
+    else {
+      link.innerHTML = fullUrl;
+    }
+  }
+  function shortenLink(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            alert(xhr.responseText);
+        }
+    }
+    xhr.open('post', 'https://www.googleapis.com/urlshortener/v1/url?' + 
+      'key=' + API_KEY + '&longUrl=' + url);
+    xhr.send()
+  }
 });
 
